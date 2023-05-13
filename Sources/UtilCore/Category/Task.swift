ecc1176d50916d0b17468e6f133829c556ab6755
zhtg <zhtg@icloud.com>
2023-05-13 21:47:35 +0800

    增加一个Task的方法

---
+++ Sources/UtilCore/Category/Task.swift
@@ -0,0 +1,25 @@
+//
+//  Task.swift
+//  NetService
+//
+//  Created by zhtg on 2023/5/13.
+//
+
+import Foundation
+
+public extension Task where Failure == Error {
+    @discardableResult
+    public init(priority: TaskPriority? = nil,
+                operation: @escaping @Sendable () async throws -> Success,
+                catch: @escaping @Sendable (Error) -> Void) {
+        self.init(priority: priority) {
+            do {
+                return try await operation()
+            }
+            catch {
+                `catch`(error)
+                throw error
+            }
+        }
+    }
+}
